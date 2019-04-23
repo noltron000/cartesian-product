4b566e92a4298c1c7a23359c5994c0e11cf01d58
Nolan Kovacik <noltron000@gmail.com>
2019-04-23 16:55:41 -0700

    cartesian product (draft code)

--- cartesian.py
+++ cartesian.py
@@ -1,4 +1,4 @@
-def cartesian_product(old_collection)
+def cartesian_product(array)
 	# this psuedocode was  helpful in determining the output
 	'''
 	# old_collection: [[1, 2, 3], [a, b, c]]
@@ -48,7 +48,74 @@ def cartesian_product(old_collection)
 	'''
 	# the loop exponentially increases in size as we go
 	# this means it is a heavy performance factor
-	pass
+
+	'''
+	Vocabulary:
+	- a collection is an array of arrays
+	- a group is an array within a collection
+	- old is the user's input
+	- new is the transformed output
+	- tmp is data not added to new for technical reasons
+
+	Variable Names:
+	- old_collection is the user's input
+	- old_group is a nested array in old_collection
+	- new_collection is the expected output
+	- new_group is a nested array in new_collection
+	- tmp_collection is data not yet added to new_collection
+	- tmp_group is data not yet added to tmp_collection
+	'''
+
+	# new_collection starts as an empty array
+	# old_collection equals the input array parameter
+	new_collection = [[]]
+	old_collection = array
+
+	# start by iterating over the groups in old_collection
+	for old_group in old_collection:
+
+		# new_collection can't be modified while in a loop
+		# tmp_collection will store a subset of new data
+		# it will be added to new_collection after the next loop
+		tmp_collection = []
+
+		# iterate over new_collections
+		# it will start with just an array with one empty array
+		# every time we go through another old_group it fills up
+		for new_group in new_collection:
+
+			# finally, look through the data in old_group
+			# this data is important;
+			#    its what we need to transfer to the new structure
+			for data in old_group:
+
+				# new_group contains a set of important data
+				# there is still yet more data to append to it...
+				# ...set tmp_group to new_group and add data to it
+				tmp_group = new_group
+				tmp_group.append(data)
+				
+				# now transfer the data to our tmp_collection
+				# it will be added to the new_collection to output
+				tmp_collection.append(tmp_group)
+			
+				# exit old_group loop
+				pass
+
+			# exit new_collection loop
+			pass
+		
+		# we are now out of the new_collection loop;
+		#    this means it is mutable again
+		# since an item can't be changed while being looped over
+		new_collection.concat(tmp_collection)
+		
+		# exit old_collection loop
+		pass
+
+	# after the loops are over, we can return the result
+	print(new_collection)
+	return new_collection
 
 def convert_group(old_group, new_group)
 	# this psuedocode was  helpful in determining the output
